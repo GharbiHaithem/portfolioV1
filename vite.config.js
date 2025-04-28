f255510d1273f8f46ddb7b9fbfb1e5a5a4bd3136
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-04-28 11:03:51 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       external: [
-        // Ajoutez ici les modules problématiques
-        'react',
-        'react-dom',
-        'react-icons',
-        // Autres dépendances qui causent des erreurs
+        'react-icons/cg',
+        // Ajoutez d'autres modules si nécessaire
       ],
     },
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-})
+  },
+});
