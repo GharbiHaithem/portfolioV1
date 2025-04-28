23b2cdd8a849f611112752954f02b5e1eb4a8bae
GharbiHaithem <gharbi.haythem1988@gmail.com>
2025-04-28 11:13:08 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-icons/cg',
-        // Ajoutez d'autres modules si nécessaire
-      ],
-    },
-  },
-});
+  resolve: {
+    alias: {
+      'react-icons': '/node_modules/react-icons'
+    }
+  }
+})
